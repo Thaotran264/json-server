76ef31de1cb472fbde2de42866402031f5b2d34f
teo <teo@gmail.com>
2021-12-31 20:25:49 +0700

    custom json server

---
+++ generate-data.js
@@ -0,0 +1,63 @@
+const faker = require("faker");
+const fs = require("fs");
+
+faker.locale = "en";
+
+// Random data
+
+function randomCategoryList(num) {
+  if (num <= 0) return [];
+  const categoryList = [];
+
+  Array.from(new Array(num)).forEach((n) => {
+    const category = {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.product(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    categoryList.push(category);
+  });
+  return categoryList;
+}
+
+const randomProductList = (categoryList, numberOfProduct) => {
+  if (numberOfProduct <= 0) return [];
+
+  const productList = [];
+  for (const category of categoryList) {
+    Array.from(new Array(numberOfProduct)).forEach(() => {
+      const product = {
+        categoryId: category.id,
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: Number.parseFloat(faker.commerce.price()),
+        description: faker.commerce.productDescription(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        thumbnail: faker.image.imageUrl(400, 400),
+      };
+
+      productList.push(product);
+    });
+  }
+  return productList;
+};
+// IIFE
+(() => {
+  const categoryList = randomCategoryList(5);
+  const productList = randomProductList(categoryList, 5);
+  const db = {
+    categories: categoryList,
+    products: productList,
+    profile: {
+      name: "RM",
+    },
+  };
+
+  fs.writeFile("db.json", JSON.stringify(db), () => {
+    console.log("Write data success!!");
+  });
+})();
